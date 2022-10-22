3230a99d4a227c8b87ad28a4824035afc5c9f128
EdgarGrcs <edgar.gricius2@gmail.com>
2022-10-22 18:01:02 +0200

    refactor code, add closure, add link to repo

--- script.js
+++ script.js
@@ -11,89 +11,6 @@ const winnerTextWindow = document.getElementById("winner-window");
 const winningText = document.querySelector(".winning-text");
 const restartButton = document.querySelector(".restart-button");
 
-restartButton.addEventListener("click", restartGame);
-
-
-
-const WINNING_COMBO = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
-
-
-const circleSymbol = "O";
-const xSymbol = "X";
-let xTurn = true;
-let counter = 0;
-
-
-function restartGame() {
-    xTurn = true;
-    winnerTextWindow.classList.remove("open");
-    grid.forEach(grid => {
-        grid.textContent = "";
-    });
-
-    grid.forEach(gridDiv => {
-        gridDiv.addEventListener("click", clickHandler, { once: true })
-    });
-    counter = 0;
-
-}
-
-
-
-
-
-
-
-//Understand how this works
-function clickHandler(e) {
-    const grid = e.target;
-    const currentSymbol = xTurn ? xSymbol : circleSymbol;
-    placeSymbol(grid, currentSymbol);
-    if (checkForWin(currentSymbol)) {
-        displayMessage(false);
-    }
-    xTurn = !xTurn;
-
-    if (counter == 9) {
-        displayMessage(true);
-    }
-
-    counter++;
-}
-
-function displayMessage(draw) {
-    if (draw) {
-        winningText.innerText = "Draw!";
-    } else {
-        winningText.innerText = `${xTurn ? "X" : "O"} Wins!`;
-    }
-    winnerTextWindow.classList.add("open");
-}
-
-function placeSymbol(grid, currentSymbol) {
-    grid.textContent = currentSymbol;
-}
-
-
-//understand how this works
-function checkForWin(currentSymbol) {
-
-    return WINNING_COMBO.some(combination => {
-        return combination.every(index => {
-            return grid[index].textContent == currentSymbol;
-        })
-    })
-}
-
 
 const player1 = () => {
     const symbol = "X";
@@ -101,21 +18,82 @@ const player1 = () => {
 };
 
 
-
 const gameBoard = (() => {
 
+    const WINNING_COMBO = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
-    return {};
+    const circleSymbol = "O";
+    const xSymbol = "X";
+    let xTurn = true;
+    let counter = 0;
+    return { circleSymbol, xSymbol, xTurn, counter, WINNING_COMBO };
 })();
 
 
 const gameFlow = (() => {
 
-    const checkGameFlow = () => {}
+    function restartGame() {
+        gameBoard.xTurn = true;
+        winnerTextWindow.classList.remove("open");
+        grid.forEach(grid => {
+            grid.textContent = "";
+        });
+
+        grid.forEach(gridDiv => {
+            gridDiv.addEventListener("click", clickHandler, { once: true })
+        });
+        gameBoard.counter = 0;
+    }
 
 
-    return {};
+    function clickHandler(e) {
+        const grid = e.target;
+        const currentSymbol = gameBoard.xTurn ? gameBoard.xSymbol : gameBoard.circleSymbol;
+        placeSymbol(grid, currentSymbol);
+        if (checkForWin(currentSymbol)) {
+            displayMessage(false);
+        }
+        gameBoard.xTurn = !gameBoard.xTurn;
+
+        if (gameBoard.counter == 9) {
+            displayMessage(true);
+        }
+        gameBoard.counter++;
+    }
+
+    function displayMessage(draw) {
+        if (draw) {
+            winningText.innerText = "Draw!";
+        } else {
+            winningText.innerText = `${gameBoard.xTurn ? "X" : "O"} Wins!`;
+        }
+        winnerTextWindow.classList.add("open");
+    }
+
+    function placeSymbol(grid, currentSymbol) {
+        grid.textContent = currentSymbol;
+    }
+
+    function checkForWin(currentSymbol) {
+        return gameBoard.WINNING_COMBO.some(combination => {
+            return combination.every(index => {
+                return grid[index].textContent == currentSymbol;
+            })
+        })
+    }
+
+    return { restartGame };
 })();
 
+restartButton.addEventListener("click", gameFlow.restartGame);
 
-restartGame();
+gameFlow.restartGame();
